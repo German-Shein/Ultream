44503c3281aefab6d65b6238b0dab9482bfa2ad9
German Shein <germanshein1995@gmail.com>
2022-03-24 23:32:22 +0400

    Implementing the "Not Found" page

--- src/Application.js
+++ src/Application.js
@@ -2,6 +2,7 @@ import './Application.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './Components/Header/Header';
 import Landing from './Pages/Landing/Landing';
+import Not_Found from './Pages/Not_Found/Not_Found';
 import Subscriptions from './Pages/Subscriptions/Subscriptions';
 import Footer from './Components/Footer/Footer';
 
@@ -13,6 +14,7 @@ const Application = () =>
 			<Routes>
 				<Route path='/' element={<Landing />} />
 				<Route path='/subscriptions' element={<Subscriptions />} />
+				<Route path='/*' element={<Not_Found />} />
 			</Routes>
 			<Footer />
 		</BrowserRouter>
---
+++ src/Pages/Not_Found/Not_Found.js
@@ -0,0 +1,12 @@
+import './Not_Found.css';
+
+const Not_Found = () =>
+{
+	return (
+		<div className="Not_Found_Page">
+			<h1>Impossible! There is something even we don't have!</h1>
+		</div>
+	);
+}
+
+export default Not_Found;
